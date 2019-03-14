04086b89fd8e01b0b8f3157142b672dd8e5a0c9b
Agustina Dinamarca <agustinadinamarca@gmail.com>
2019-03-14 20:04:44 -0300

    Update tsne-embedding.py

--- tsne-embedding.py
+++ tsne-embedding.py
@@ -7,7 +7,7 @@ import numpy as np
 
 cwd = os.getcwd() 
 
-
+# Red rattus norvegicus como ejemplo
 fname = cwd + '/embeddings-normalized/w10-80walks-40l-d128-rattus_norvegicus_norm'
 data = pd.read_csv(fname, delimiter=' ', header=None)
 
@@ -22,7 +22,7 @@ X_2d = tsne.fit_transform(X)
 from matplotlib import pyplot as plt
 plt.figure(figsize=(6, 5))
 for i in range(len(X_2d)):
-	plt.scatter(X_2d[i, 0], X_2d[i, 1])
+	plt.scatter(X_2d[i, 0], X_2d[i, 1], s=2, c='red', alpha=0.5)
 
 plt.savefig(str(cwd)+'/tsne-rattus-d128.pdf', format='pdf')
 
