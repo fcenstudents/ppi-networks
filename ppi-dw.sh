364afab5258cce5a9d9b3f38ed03e29d5492789c
Agustina Dinamarca <agustinadinamarca@gmail.com>
2019-05-03 11:05:31 -0300

    Update ppi-dw.sh

--- ppi-dw.sh
+++ ppi-dw.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-
-
 f='deepwalk-embeddings/w10-80walks-40l-d128-'
 b='.txt'
-
-
 g='data/'
-
-
-
 X=("arab" "coli" "drosop" "elegans" "human" "inter_H-Y" "mus_musculus" "rattus_norvegicus" "yeast")
-
-
 
 for w in {0..9}
 do
@@ -20,9 +11,3 @@ do
 	deepwalk --format "edgelist" --input $g${X[$w]}$b --max-memory-data-size 0 --number-walks 80 --representation-size 128 --walk-length 40 --window-size 10 --seed 0 --workers 4 --output $d
 
 done
-
-
-
-
-
-
