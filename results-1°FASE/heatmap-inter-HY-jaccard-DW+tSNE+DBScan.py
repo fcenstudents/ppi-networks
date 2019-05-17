e96b66a7eed31061e0fc8fe561053727c6199b3b
Agustina Dinamarca <agustinadinamarca@gmail.com>
2019-05-17 12:27:55 -0300

    Add files via upload

---
+++ results-1°FASE/heatmap-inter-HY-jaccard-DW+tSNE+DBScan.py
@@ -0,0 +1,62 @@
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+from math import*
+import seaborn as sns
+import pandas as pd
+
+  
+def jaccard_similarity(x, y):
+	intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
+	union_cardinality = len(set.union(*[set(x), set(y)]))
+	return intersection_cardinality/float(union_cardinality)
+
+### RED INTER HY
+
+cwd = os.getcwd() 
+
+
+### ahora implemento un loop sobre los archivos que contienen los clusters por dimensiones de DW+tSNE+DBSCAN
+
+dim = [2, 10, 20, 50, 80, 100, 128, 300, 500]
+
+
+fig = plt.figure(figsize=(15,18))
+
+for j in range(len(dim)-1):
+	fnamei = cwd + "/clustering-deepwalk+tsne/c-dbscan-tsne-w10-80walks-40l-d" + str(dim[j]) + "-inter_H-Y_.txt"
+	fnameii = cwd + "/clustering-deepwalk+tsne/c-dbscan-tsne-w10-80walks-40l-d" + str(dim[j+1]) + "-inter_H-Y_.txt"
+	lsti = []
+	lstii = []
+	J = open(fnamei, 'r')   
+	for line in J:
+		lsti.append(list(map(int, line.split())))
+	J.close()
+	lsti.sort(key=len, reverse=True)
+	J = open(fnameii, 'r')   
+	for line in J:
+		lstii.append(list(map(int, line.split())))
+	J.close()
+	lstii.sort(key=len, reverse=True)
+	labelsi = []
+	labelsii = []
+	for t in range(1, len(lsti)+1):
+		labelsi.append('c'+str(t)+'-d'+str(dim[j]))
+	for s in range(1, len(lstii)+1):
+		labelsii.append('c'+str(s)+'-d'+str(dim[j+1]))
+	jacc = np.zeros((len(lsti), len(lstii)))
+	for w in range(len(lsti)):
+		for f in range(len(lstii)):
+			jacc[w, f] = jaccard_similarity(lstii[f], lsti[w])
+	
+	df = pd.DataFrame(jacc, columns=labelsii, index=labelsi)
+	plt.subplot(2,4,j+1)
+	sns.heatmap(df, vmin=0, vmax=1, cmap="YlGnBu", annot=True, fmt='.2f')
+	plt.yticks(rotation=0) 
+	plt.title("J: DIM "+str(dim[j])+' vs. '+str(dim[j+1])+" - INTER-HY")
+
+
+fig.tight_layout()
+plt.savefig(cwd + '/result-heatmap-interHY-DW+tSNE+DBScan.png', format='png')
